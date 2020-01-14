68dd304cccf6c7bac81623b6df6010735569f3e7
achanajnr <achanamosey@gmail.com>
2020-01-14 16:17:08 +0000

    Created the Express middleware that handles the authentication process.

--- book.js
+++ book.js
@@ -41,3 +41,24 @@ const books = [
 app.get('/books', (req, res) => {
     res.json(books);
 });
+
+const accessTokenSecret = 'youraccesstokensecret';
+
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
